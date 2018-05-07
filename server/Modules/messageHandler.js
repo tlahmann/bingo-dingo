b91d59ea61440ba603a5f7e4ad9aecaf2a573280
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2018-05-07 22:21:24 +0200

    Filter inactive users and old numbers in stats

--- server/Modules/messageHandler.js
+++ server/Modules/messageHandler.js
@@ -67,36 +67,38 @@ export default class MessageHandler {
 
   sendStats () {
     let stats = {}
-    stats['usersOnline'] = this.gm.users.length
+    let users = this.gm.users.filter(u => u.nickname)
+    stats['usersOnline'] = users.length
     stats['numbersDrawn'] = this.gm.numbers.length
-    stats['completion'] = this.gm.users.reduce((s, u) => s + u.board.filter(b => b.isClicked).length, 0)
-    stats['misses'] = this.gm.users
+    stats['completion'] = users.reduce((s, u) => s + u.board.filter(b => b.isClicked).length, 0)
+    stats['misses'] = users
       .reduce((s, u) =>
         s + u.board.filter(f =>
         !f.isClicked &&
+        (new Date).getTime() - f.timestamp < 2 * 60 * 60 * 1000 &&
         this.gm.numbers.some(n =>
-          n.number == f.number
+          +n.number === +f.number
         )).length,
         0)
-    stats['bingos'] = this.gm.users.reduce((s, u) => s + u.bingos, 0)
+    stats['bingos'] = users.reduce((s, u) => s + u.bingos, 0)
     // TODO: get number of plyers that need one, two, three. etc for a bingo
-    stats['oneToGo'] = this.gm.users
+    stats['oneToGo'] = users
       .reduce((s, u) =>
         s + (hf.countMissing(u.board, u.lines, 4) || 0),
         0)
-    stats['twoToGo'] = this.gm.users
+    stats['twoToGo'] = users
       .reduce((s, u) =>
         s + (hf.countMissing(u.board, u.lines, 3) || 0),
         0)
     let nums = new Array(75).fill(0)
-    this.gm.users.forEach(u => {
+    users.forEach(u => {
       u.board.forEach(n => {
         if (n.number != 'FREE')
           nums[n.number]++
       })
     })
     stats['common'] = hf.findCommon(nums, this.gm.numbers)
-    stats['bingosPlayers'] = this.gm.users.filter(u => u.bingos > 0).map(u => {
+    stats['bingosPlayers'] = users.filter(u => u.bingos > 0).map(u => {
       return {id: u.id, nickname: u.nickname}
     })
     this.broadcast(p.MESSAGE_STATS, stats)
